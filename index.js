5d3bbc03238e5cf6718eca22908022a26253eed2
Mike <mreickcastillero@gmail.com>
2023-10-28 18:10:23 -0700

    added other view functions to inquirer

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
-const PORT = process.env.PORT||3001;
+const PORT = process.env.PORT || 3001;
 const db = mysql.createConnection(
     {
         host: 'localhost',
@@ -11,24 +11,50 @@ const db = mysql.createConnection(
     console.log(`Connected to the company_db database.`)
 );
 
-inquirer
-    .prompt({
-        type: "list",
-        message: "What would you like to do?",
-        choices: ["View all departments", "View all roles", "View all Employees", "add a department", "Add a role", "Add an employee", "Update an employee role"],
-        name: "Action"
 
-    })
-.then((response) => {
-    console.log(response);
-    if(response.Action === "View all departments"){
-db.query("SELECT * FROM department", function (err, results) {
-    if (err){
-        console.log(err);
-    }
-    console.log("Department", results);
-})};
-})
+function startQuestions() {
+    inquirer
+        .prompt({
+            type: "list",
+            message: "What would you like to do?",
+            choices: ["View all departments", "View all roles", "View all Employees", "add a department", "Add a role", "Add an employee", "Update an employee role"],
+            name: "Action"
+
+        })
+        .then((response) => {
+            console.log(response);
+            if (response.Action === "View all departments") {
+                db.query("SELECT * FROM department", function (err, results) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    console.table(results);
+                    startQuestions();
+                })
+            };
+            if (response.Action === "View all roles") {
+                db.query("SELECT * FROM roles", function (err, results) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    console.table(results);
+                    startQuestions();
+                })
+            }
+            if (response.Action === "View all Employees") {
+                db.query("SELECT * FROM employee", function (err, results) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    console.table(results);
+                    startQuestions();
+                })
+            }
+        })
+
+}
+
+startQuestions();
 
 
 
